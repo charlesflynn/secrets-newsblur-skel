3eb6ed868cb3a0865ba773e69d6839d34ce10c76
Charles Flynn <cflynn@irab.org>
2013-05-04 11:47:20 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# newsblur-cfg-skeleton
+Skeleton for the secrets-newsblur dir required for installation
---
+++ settings/app_settings.py
@@ -0,0 +1,123 @@
+import logging
+import pymongo
+
+# ===================
+# = Server Settings =
+# ===================
+
+ADMINS                = (
+    ('Samuel Clay', 'cflynn@irab.org'),
+)
+
+SERVER_EMAIL          = 'server@irab.org'
+HELLO_EMAIL           = 'hello@irab.org'
+NEWSBLUR_URL          = 'http://10.100.100.11'
+SESSION_COOKIE_DOMAIN = '.localhost'
+
+# ===================
+# = Global Settings =
+# ===================
+
+DEBUG = True
+DEBUG_ASSETS = DEBUG
+MEDIA_URL = '/media/'
+SECRET_KEY = 'YOUR SECRET KEY'
+
+# CACHE_BACKEND = 'dummy:///'
+# CACHE_BACKEND = 'locmem:///'
+# CACHE_BACKEND = 'memcached://127.0.0.1:11211'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.RedisCache',
+        'LOCATION': '127.0.0.1:6379',
+        'OPTIONS': {
+            'DB': 6,
+            'PARSER_CLASS': 'redis.connection.HiredisParser'
+        },
+    },
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Set this to the username that is shown on the homepage to unauthenticated users.
+HOMEPAGE_USERNAME = 'popular'
+
+# Google Reader OAuth API Keys
+OAUTH_KEY = 'www.example.com'
+OAUTH_SECRET = 'SECRET_KEY_FROM_GOOGLE'
+
+S3_ACCESS_KEY = 'XXX'
+S3_SECRET = 'SECRET'
+S3_BACKUP_BUCKET = 'newsblur_backups'
+S3_PAGES_BUCKET_NAME = 'pages-XXX.newsblur.com'
+S3_ICONS_BUCKET_NAME = 'icons-XXX.newsblur.com'
+
+STRIPE_SECRET = "YOUR-SECRET-API-KEY"
+STRIPE_PUBLISHABLE = "YOUR-PUBLISHABLE-API-KEY"
+
+# ===============
+# = Social APIs =
+# ===============
+
+FACEBOOK_APP_ID = '111111111111111'
+FACEBOOK_SECRET = '99999999999999999999999999999999'
+TWITTER_CONSUMER_KEY = 'ooooooooooooooooooooo'
+TWITTER_CONSUMER_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+# =============
+# = Databases =
+# =============
+
+DATABASES = {
+    'default': {
+        'NAME': 'newsblur',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'newsblur',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+    },
+}
+
+MONGO_DB = {
+    'name': 'newsblur',
+    'host': '127.0.0.1',
+    'port': 27017
+}
+
+MONGODB_SLAVE = {
+    'host': '127.0.0.1'
+}
+
+# Celery RabbitMQ/Redis Broker
+BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_RESULT_BACKEND = BROKER_URL
+
+REDIS = {
+    'host': '127.0.0.1',
+}
+
+ELASTICSEARCH_HOSTS = ['127.0.0.1:9200']
+
+BACKED_BY_AWS = {
+    'pages_on_node': False,
+    'pages_on_s3': False,
+    'icons_on_s3': False,
+}
+
+ORIGINAL_PAGE_SERVER = "127.0.0.1:3060"
+
+# ===========
+# = Logging =
+# ===========
+
+# Logging (setup for development)
+LOG_TO_STREAM = True
+
+if len(logging._handlerList) < 1:
+    LOG_FILE = '~/newsblur/logs/development.log'
+    logging.basicConfig(level=logging.DEBUG,
+                            format='%(asctime)-12s: %(message)s',
+                            datefmt='%b %d %H:%M:%S',
+                            handler=logging.StreamHandler)
+
